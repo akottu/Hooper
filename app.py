48b08f84c9ed9a5bf8f8fed89ff5087e51efab66
Allen Zhao <xuan.zhao@duke.edu>
2020-04-23 10:59:34 -0400

    edit page debug

--- app.py
+++ app.py
@@ -103,22 +103,6 @@ def edit_player(name):
     else:
         return render_template('edit-player.html', player=player, form=form)
 
-@app.route('/edit-player/<name>', methods=['GET', 'POST'])
-def edit_player(name):
-    player = db.session.query(models.Players)\
-        .filter(models.Players.name == name).first()
-    form = forms.PlayerEditFormFactory.form(player)
-    if form.validate_on_submit():
-        try:
-            form.errors.pop('database', None)
-            player.player_id = form.player_id.data
-            db.session.commit()
-            return redirect(url_for('view_players'))
-        except BaseException as e:
-            form.errors['database'] = str(e)
-            return render_template('edit-player.html', player=player, form=form)
-    else:
-        return render_template('edit-player.html', player=player, form=form)
 
 @app.route('/edit-team/<name>', methods=['GET', 'POST'])
 def edit_team(name):
--- forms.py
+++ forms.py
@@ -1,16 +1,59 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, IntegerField, SelectField, SubmitField
+from wtforms import StringField, BooleanField, IntegerField, SelectField, SubmitField, DecimalField
 from wtforms.validators import DataRequired
+from wtforms_components import TimeField
 
 class PlayerEditFormFactory:
     @staticmethod
     def form(player):
         class F(FlaskForm):
             name = StringField(default=player.name)
-            age = StringField(default=player.age)
+            player_id = IntegerField(default=player.player_id)
             position = StringField(default=player.position)
-            team = StringField(default=player.team_id)
+            avg_points = DecimalField(default=player.avg_points)
+            avg_rebounds = DecimalField(default=player.avg_rebounds)
+            avg_assists = DecimalField(default=player.avg_assists)
+            avg_steals = DecimalField(default=player.avg_steals)
+            avg_blocks = DecimalField(default=player.avg_blocks)
+            avg_turnovers = DecimalField(default=player.avg_turnovers)
+            avg_fgm = DecimalField(default=player.avg_fgm)
+            avg_fga = DecimalField(default=player.avg_fga)
+            avg_ftm = DecimalField(default=player.avg_ftm)
+            avg_fta = DecimalField(default=player.avg_fta)
+            eff = DecimalField(default=player.eff)
         return F()
+
+class TeamEditFormFactory:
+    @staticmethod
+    def form(team):
+        class F(FlaskForm):
+            team_id = IntegerField(default=team.team_id)
+            abbreviation = StringField(default=team.abbreviation)
+            nickname = StringField(default=team.nickname)
+            city = StringField(default=team.city)
+            owner = StringField(default=team.owner)
+            general_manager = StringField(default=team.general_manager)
+            head_coach = StringField(default=team.head_coach)
+            conference =StringField(default=team.conference)
+            w = IntegerField(default=team.w)
+            l = IntegerField(default=team.l)
+            gp = IntegerField(default=team.w + team.l)
+            w_pct = DecimalField(default=team.w_pct)
+        return F()
+
+class GameEditFormFactory:
+    @staticmethod
+    def form(game):
+        class F(FlaskForm):
+            game_date_est = DateField(default=game.game_date_est)
+            game_time = TimeField(default=game.game_time)
+            game_id = IntegerField(default=game.game_id)
+            game_status_text = StringField(default=game.game_status_text)
+        return F()
+
+
+
+'''
 
 
 class DrinkerEditFormFactory:
@@ -59,3 +102,4 @@ class ServingsFormFactory:
             beer_sel = SelectField('Beer Name', choices= beer_names )
             submit = SubmitField('Submit')
         return F()
+'''
