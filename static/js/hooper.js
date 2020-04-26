0ab21b1528d7272ddadd0db921604223fd706843
Anish <anish.kottu@duke.edu>
2020-04-26 15:49:17 -0400

    added js

--- static/js/hooper.js
+++ static/js/hooper.js
@@ -2,7 +2,10 @@ function editPlayer(player) {
     alert("Editing player with id " + player.player_id)
 }
 
-if($("#home-points").text() > $("#away-points").text()) {
-    $("#home-team").css("background-color", "lightblue")
-    $("#home-stats").css("background-color", "lightblue")
+if($(".home-points").text() > $(".away-points").text()) {
+    $(".home-team").css("background-color", "lightblue")
+    $(".home-stats").css("background-color", "lightblue")
+} else {
+    $(".away-team").css("background-color", "lightblue")
+    $(".away-stats").css("background-color", "lightblue")
 }
