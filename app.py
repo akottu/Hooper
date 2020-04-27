b8d905aa96ae1a1721298507c38f06e3b661e6b7
Diego Sosa Fundora <ds416@duke.edu>
2020-04-26 22:02:31 -0400

    clean up

--- app.py
+++ app.py
@@ -268,50 +268,6 @@ def edit_game(gameID):
     else:
         return render_template('edit-game.html', game=game, form=form)
 
-
-@app.route('/drinker/<name>')
-def drinker(name):
-    drinker = db.session.query(models.Drinker)\
-        .filter(models.Drinker.name == name).one()
-    return render_template('drinker.html', drinker=drinker)
-
-@app.route('/serves', methods=['GET', 'POST'])
-def serves():
-    beers = db.session.query(models.Beer).all()
-    beer_names = [beer.name for beer in beers]
-    form = forms.ServingsFormFactory.form(beer_names)
-    if form.validate_on_submit():
-        return render_template('/servings/' + form.beer_sel.data)
-    return render_template('serves.html', form=form)
-
-@app.route('/servings/<beer_name>', methods=['GET', 'POST'])
-def servings_for(beer_name):
-    selected_beer = request.args.get('list_status')
-    results = db.session.query(models.Serves, models.Bar) \
-                .filter(models.Serves.beer == beer_name) \
-                .filter(models.Serves.bar == models.Bar.name) \
-                .all()
-    return render_template('servings_for.html', beer_name=beer_name, data=results)
-
-@app.route('/edit-drinker/<name>', methods=['GET', 'POST'])
-def edit_drinker(name):
-    drinker = db.session.query(models.Drinker)\
-        .filter(models.Drinker.name == name).one()
-    beers = db.session.query(models.Beer).all()
-    bars = db.session.query(models.Bar).all()
-    form = forms.DrinkerEditFormFactory.form(drinker, beers, bars)
-    if form.validate_on_submit():
-        try:
-            form.errors.pop('database', None)
-            models.Drinker.edit(name, form.name.data, form.address.data,
-                                form.get_beers_liked(), form.get_bars_frequented())
-            return redirect(url_for('drinker', name=form.name.data))
-        except BaseException as e:
-            form.errors['database'] = str(e)
-            return render_template('edit-drinker.html', drinker=drinker, form=form)
-    else:
-        return render_template('edit-drinker.html', drinker=drinker, form=form)
-
 @app.template_filter('pluralize')
 def pluralize(number, singular='', plural='s'):
     return singular if number in (0, 1) else plural
